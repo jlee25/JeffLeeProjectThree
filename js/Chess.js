d925bb07720117135da289301bec7b412cbd98e8
Jeff <j_lee92@hotmail.com>
2018-08-18 21:40:08 -0400

    Added Cpu randomness

--- js/Chess.js
+++ js/Chess.js
@@ -8,13 +8,15 @@
 // Array.includes to check if the winning combination is fufilled.
 
 
-
 const tictactoe = {}
 
 tictactoe.human1 = [];
 tictactoe.human2 = [];
 tictactoe.ai = []
 tictactoe.boardstate = [];
+tictactoe.turn = 0;
+tictactoe.p1score = 1;
+tictactoe.p2score = 1;
 
 tictactoe.winningCombination = [
     [1, 2, 3],
@@ -27,249 +29,274 @@ tictactoe.winningCombination = [
     [3, 5, 7]
 ];
 
-$(function () {
-
-$('h1').each(function(){
+tictactoe.title = $('h1').each(function () { // ALTERNATING COLOUR IN THE MAIN HEADER
     let letters = $(this).text();
     let newLetters = "";
-    console.log(letters)
     for (i = 0; i < letters.length; i++) {
         if (i % 2 == 0) {
-        newLetters += (`<span class = red>${letters.charAt(i)}</span>`);  
+            newLetters += (`<span class = red>${letters.charAt(i)}</span>`);
         }
-      else
-        newLetters +=(`<span class = blue>${letters.charAt(i)}</span>`);
-        }
-        $(this).html(newLetters);
-        });
+        else
+            newLetters += (`<span class = blue>${letters.charAt(i)}</span>`);
+    }
+    $(this).html(newLetters);
+});
 
-for (i = 1; i <= 9; i++) {
+for (i = 1; i <= 9; i++) { // CREATING THE BOARD
     $('.board').append(`<div class="square square${i}" data-value="${i}"></div>`)
 };
 
-let players = ["player1","player2"];
-let pieces = ["X","O"];
-let turn = 0;
-let p1score = 1;
-let p2score = 1;
-
-let switchturns = function () {
-    if (turn == 0) {
-        turn = 1;
+tictactoe.switchturns = function () { // SWITCHING TURNS
+    if (tictactoe.turn == 0) {
+        tictactoe.turn = 1;
     } else {
-        turn = 0;
+        tictactoe.turn = 0;
     }
 }
 
-let humanvshuman = $('.human').on('click', function() {
-let placingPiece = $('.board').on('click', '.square', function() {
-    if (turn == 0) {
-        if ($(this).hasClass('x-icon') || ($(this).hasClass('o-icon'))) {
-            alert('Do you know how to play this game? Click another square!!!');
-            $(this).notaddClass("x-icon")
-    }
-    else {
-        $(this).addClass("x-icon");
-        let number = $(this).data("value");
-        tictactoe.human1.push(number);
-        tictactoe.boardstate.push(number);
-        turnspeech();
-        console.log(tictactoe.human1);
-        console.log(tictactoe.boardstate);
-        checkForWin();
-        checkForDraw();
-    }
-    }
-    else {
-        if ($(this).hasClass('x-icon') || ($(this).hasClass('o-icon'))) {
-            alert('Do you know how to play this game? Click another square!!!');
-            $(this).notaddClass("o-icon")
-    } 
-    else {
-        $(this).addClass("o-icon");
-        let number = $(this).data("value");
-        tictactoe.human2.push(number);
-        tictactoe.boardstate.push(number);
-        console.log(tictactoe.boardstate);
-        turnspeech();
-        console.log(tictactoe.human2);
-        checkForWin2();
-        checkForDraw();
-    }
-    }
-    switchturns();
-});
+// HUMAN VS HUMAN (PLACING A PIECE AND ALL OTHER FUNCTIONS INCLUDED)
+
+tictactoe.humanvshuman = $('.human').on('click', function () { 
+    tictactoe.placingPiece = $('.board').on('click', '.square', function () {
+        if (tictactoe.turn == 0) {
+            if ($(this).hasClass('x-icon') || ($(this).hasClass('o-icon'))) {
+                alert('Do you know how to play this game? Click another square!!!');
+                $(this).notaddClass("x-icon")
+            }
+            else {
+                $(this).addClass("x-icon");
+                let number = $(this).data("value");
+                tictactoe.human1.push(number);
+                tictactoe.boardstate.push(number);
+                tictactoe.turnspeech();
+                tictactoe.checkForWin();
+                tictactoe.checkForDraw();
+            }
+        }
+        else {
+            if ($(this).hasClass('x-icon') || ($(this).hasClass('o-icon'))) {
+                alert('Do you know how to play this game? Click another square!!!');
+                $(this).notaddClass("o-icon")
+            }
+            else {
+                $(this).addClass("o-icon");
+                let number = $(this).data("value");
+                tictactoe.human2.push(number);
+                tictactoe.boardstate.push(number);
+                tictactoe.turnspeech();
+                tictactoe.checkForWin2();
+                tictactoe.checkForDraw();
+            }
+        }
+        tictactoe.switchturns();
+    });
 });
 
-// Against Computer
+// AGAINST COMPUTER
 
-let humanvscomputer = $('.computer').on('click', function () {
-let placingPiece2 = $('.board').on('click', '.square', function () {
-    if (turn == 0) {
+tictactoe.humanvscomputer = $('.computer').on('click', function () {
+    tictactoe.placingPiece2 = $('.board').on('click', '.square', function () {
+        $('h2').html("You can do it!!!!!")
         if ($(this).hasClass('x-icon') || ($(this).hasClass('o-icon'))) {
             alert('Do you know how to play this game? Click another square!!!');
             $(this).notaddClass("x-icon")
         }
         else {
             $(this).addClass("x-icon");
+            $(this).addClass("filled");
             let number = $(this).data("value");
             tictactoe.human1.push(number);
             tictactoe.boardstate.push(number);
-            console.log(tictactoe.human1);
-            console.log(tictactoe.boardstate);
-            checkForWin();
-            checkForDraw();
+            tictactoe.checkForWin();
+            tictactoe.checkForDraw();
+            tictactoe.cputurn();
         }
-    }
-
     });
 });
 
-let computerplays = function() {
-    let cpu = Math.floor(Math.random() * 9) + 1;
+// Generate a random number to place in a square.
+// Check if square does not have a piece already //
+// Place in random square //
+// If 2 out of 3 squares have a piece, place in the last square.
+
+
+tictactoe.squareOpen = function (dataValue) { // CHECKING IF SQUARE HAS A PIECE ON IT
+    if ($(`.square${dataValue}`).hasClass("filled")) {
+        squareFree = false;
+    }
+    else {
+        squareFree = true;
+    }
 }
 
-const checkForWin = function() {
+tictactoe.randomPlay = function () { // MAKING CPU ADD A RANDOM PIECE TO AN EMPTY SPOT
+    for (i = 1; i <= 30; i++) {
+        let rand = Math.floor(Math.random() * 9) + 1;
+        tictactoe.squareOpen(rand);
+        if (squareFree == true) {
+            $(`.square${rand}`).addClass("o-icon");
+            $(`.square${rand}`).addClass("filled");
+            tictactoe.human2.push(rand);
+            tictactoe.boardstate.push(rand);
+            break;
+        }
+    }
+}
+
+tictactoe.smartPlay = function () { // If either person is about to win. Put the O there.
+    if ($('.square1').hasClass("filled") && ($('.square2').hasClass("filled"))) {
+        $('.square3').addClass("filled").addClass("x-icon");
+    }
+}
+
+tictactoe.cputurn = function () { // CPU COMPLETE TURN
+    tictactoe.randomPlay();
+    tictactoe.checkForWin2();
+    tictactoe.checkForDraw();
+}
+
+
+tictactoe.checkForWin = function () { // CHECKING FOR A WIN. WOULD LOVE TO FIND A LOOP FOR IT BECAUSE I KNOW THERE IS!! 
     if ($(".square1").hasClass("x-icon") && $(".square2").hasClass("x-icon") && $(".square3").hasClass("x-icon")) {
         alert("Player 1 has won");
-        $('.p1 .counter').html(`${p1score++}`)
+        $('.p1 .counter').html(`${tictactoe.p1score++}`)
         tictactoe.boardstate = [];
-        resetboard();
+        tictactoe.resetboard()
         turn = 0
-    }    
+    }
     else if ($(".square4").hasClass("x-icon") && $(".square5").hasClass("x-icon") && $(".square6").hasClass("x-icon")) {
         alert("Player 1 has won");
-        $('.p1 .counter').html(`${p1score++}`)
+        $('.p1 .counter').html(`${tictactoe.p1score++}`)
         tictactoe.boardstate = [];
-        resetboard();
+        tictactoe.resetboard()
         turn = 0
-    } 
+    }
     else if ($(".square7").hasClass("x-icon") && $(".square8").hasClass("x-icon") && $(".square9").hasClass("x-icon")) {
         alert("Player 1 has won");
-        $('.p1 .counter').html(`${p1score++}`)
+        $('.p1 .counter').html(`${tictactoe.p1score++}`)
         tictactoe.boardstate = [];
-        resetboard();
+        tictactoe.resetboard()
         turn = 0
-    }   
+    }
     else if ($(".square1").hasClass("x-icon") && $(".square4").hasClass("x-icon") && $(".square7").hasClass("x-icon")) {
         alert("Player 1 has won");
-        $('.p1 .counter').html(`${p1score++}`)
+        $('.p1 .counter').html(`${tictactoe.p1score++}`)
         tictactoe.boardstate = [];
-        resetboard();
+        tictactoe.resetboard()
         turn = 0
-    }       
+    }
     else if ($(".square2").hasClass("x-icon") && $(".square5").hasClass("x-icon") && $(".square8").hasClass("x-icon")) {
         alert("Player 1 has won");
-        $('.p1 .counter').html(`${p1score++}`)
+        $('.p1 .counter').html(`${tictactoe.p1score++}`)
         tictactoe.boardstate = [];
-        resetboard();
+        tictactoe.resetboard()
         turn = 0
-    }       
+    }
     else if ($(".square3").hasClass("x-icon") && $(".square6").hasClass("x-icon") && $(".square9").hasClass("x-icon")) {
         alert("Player 1 has won");
-        $('.p1 .counter').html(`${p1score++}`)
+        $('.p1 .counter').html(`${tictactoe.p1score++}`)
         tictactoe.boardstate = [];
-        resetboard();
+        tictactoe.resetboard()
         turn = 0
-    }       
+    }
     else if ($(".square1").hasClass("x-icon") && $(".square5").hasClass("x-icon") && $(".square9").hasClass("x-icon")) {
         alert("Player 1 has won");
-        $('.p1 .counter').html(`${p1score++}`)
+        $('.p1 .counter').html(`${tictactoe.p1score++}`)
         tictactoe.boardstate = [];
-        resetboard();
+        tictactoe.resetboard()
         turn = 0
-    }       
+    }
     else if ($(".square3").hasClass("x-icon") && $(".square5").hasClass("x-icon") && $(".square7").hasClass("x-icon")) {
         alert("Player 1 has won");
-        $('.p1 .counter').html(`${p1score++}`)
+        $('.p1 .counter').html(`${tictactoe.p1score++}`)
         tictactoe.boardstate = [];
-        resetboard();
+        tictactoe.resetboard()
         turn = 0
-    }          
+    }
     else return ("false");
 }
 
-const checkForWin2 = function() {
+tictactoe.checkForWin2 = function () { // CHECKING FOR WIN FOR SECOND PLAYER
     if ($(".square1").hasClass("o-icon") && $(".square2").hasClass("o-icon") && $(".square3").hasClass("o-icon")) {
         alert("Player 2 has won");
-        $('.p2 .counter').html(`${p2score++}`)
+        $('.p2 .counter').html(`${tictactoe.p2score++}`)
         tictactoe.boardstate = [];
-        resetboard();
+        tictactoe.resetboard()
         turn = 1
     }
     else if ($(".square4").hasClass("o-icon") && $(".square5").hasClass("o-icon") && $(".square6").hasClass("o-icon")) {
         alert("Player 2 has won");
-        $('.p2 .counter').html(`${p2score++}`)
+        $('.p2 .counter').html(`${tictactoe.p2score++}`)
         tictactoe.boardstate = [];
-        resetboard();
+        tictactoe.resetboard()
         turn = 1
     }
     else if ($(".square7").hasClass("o-icon") && $(".square8").hasClass("o-icon") && $(".square9").hasClass("o-icon")) {
         alert("Player 2 has won");
-        $('.p2 .counter').html(`${p2score++}`)
+        $('.p2 .counter').html(`${tictactoe.p2score++}`)
         tictactoe.boardstate = [];
-        resetboard();
+        tictactoe.resetboard()
         turn = 1
     }
     else if ($(".square1").hasClass("o-icon") && $(".square4").hasClass("o-icon") && $(".square7").hasClass("o-icon")) {
         alert("Player 2 has won");
-        $('.p2 .counter').html(`${p2score++}`)
+        $('.p2 .counter').html(`${tictactoe.p2score++}`)
         tictactoe.boardstate = [];
-        resetboard();
+        tictactoe.resetboard()
         turn = 1
     }
     else if ($(".square2").hasClass("o-icon") && $(".square5").hasClass("o-icon") && $(".square8").hasClass("o-icon")) {
         alert("Player 2 has won");
-        $('.p2 .counter').html(`${p2score++}`)
+        $('.p2 .counter').html(`${tictactoe.p2score++}`)
         tictactoe.boardstate = [];
-        resetboard();
+        tictactoe.resetboard()
         turn = 1
     }
     else if ($(".square3").hasClass("o-icon") && $(".square6").hasClass("o-icon") && $(".square9").hasClass("o-icon")) {
         alert("Player 2 has won");
-        $('.p2 .counter').html(`${p2score++}`)
+        $('.p2 .counter').html(`${tictactoe.p2score++}`)
         tictactoe.boardstate = [];
-        resetboard();
+        tictactoe.resetboard()
         turn = 1
     }
     else if ($(".square1").hasClass("o-icon") && $(".square5").hasClass("o-icon") && $(".square9").hasClass("o-icon")) {
         alert("Player 2 has won");
-        $('.p2 .counter').html(`${p2score++}`)
+        $('.p2 .counter').html(`${tictactoe.p2score++}`)
         tictactoe.boardstate = [];
-        resetboard();
+        tictactoe.resetboard()
         turn = 1
     }
     else if ($(".square3").hasClass("o-icon") && $(".square5").hasClass("o-icon") && $(".square7").hasClass("o-icon")) {
         alert("Player 2 has won");
-        $('.p2 .counter').html(`${p2score++}`)
+        $('.p2 .counter').html(`${tictactoe.p2score++}`)
         tictactoe.boardstate = [];
-        resetboard();
+        tictactoe.resetboard()
         turn = 1
-    }   
-    else return ("false");    
+    }
+    else return ("false");
 }
 
-const resetboard = function() { // Reset Board Function
+tictactoe.resetboard = function () { // RESET BOARD FUNCTION
     for (i = 1; i <= 9; i++) {
-    $(`.square${i}`).removeClass("x-icon").removeClass("o-icon");
-    $('h2').html("Please click on a square to begin")
+        $(`.square${i}`).removeClass("x-icon").removeClass("o-icon").removeClass("filled");
+        $('h2').html("Please click on a square to begin")
     }
 }
 
-const checkForDraw = function() {
+tictactoe.checkForDraw = function () { // CHECKING FOR A DRAW
     if (tictactoe.boardstate.length == 9) {
         alert("Its a draw. WOW! you two are equally skilled!");
         tictactoe.boardstate = [];
-        resetboard();
+        tictactoe.resetboard()
         $('h2').html("Please click on a square to begin")
     }
 }
 
 
-const resetbutton = $('section button').on('click', function () { // Reset Button at the bottom of the screen
+tictactoe.resetbutton = $('section button').on('click', function () { // RESET BUTTON ON BOTTOM OF THE SCREEN
     let cheating = confirm("Are you sure you want to reset the board? Please ask the other player before you cheat.");
     if (cheating == true) {
-        $('div').removeClass("x-icon").removeClass("o-icon");
+        $('div').removeClass("x-icon").removeClass("o-icon").removeClass("filled");
         $('h2').html("Please click on a square to begin")
         tictactoe.boardstate = [];
     }
@@ -278,53 +305,24 @@ const resetbutton = $('section button').on('click', function () { // Reset Butto
     }
 });
 
-const turnspeech = function() {
-    if (tictactoe.boardstate.length % 2 == 0 || turn == 1) {
+tictactoe.turnspeech = function () { // H2 THAT CHANGES DEPENDING ON WHOSE TURN IT IS
+    if (tictactoe.boardstate.length % 2 == 0 || tictactoe.turn == 1) {
         $('h2').html("It's Player One's <span class = blue>X Turn!</span>")
     }
-    if (turn == 0) {
+    if (tictactoe.turn == 0) {
         $('h2').html("It's Player Two's <span class = red>O Turn!</span>")
     }
 }
 
-let winningCombination = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9],
-    [1, 4, 7],
-    [2, 5, 8],
-    [3, 6, 9],
-    [1, 5, 9],
-    [3, 5, 7]
-];
-
-let transition = $(".human, .computer").click(function () {
-    $('header').fadeOut(2000); 
+tictactoe.transition = $(".human, .computer").click(function () { // FADING OUT WHEN ENTERING GAME.
+    $('header').fadeOut(2000);
 });
 
-// let cpuPlacingPiece = function () {
-//     let cpu = Math.floor(Math.random() * 9) + 1;
-//     console.log(cpu)
-//     if ((`square${cpu}`) == (`square${i}`)) {
-//         $(`square${i}`).addClass("o-icon");
 
-//     }
-// }
+tictactoe.init = function () {
 
+}
 
-
-// let cpuPlacingPiece = function() {
-//     for (i = 1; i <= 1000; i++) {
-//         if 
-//     }
-// }
-
-// const draw = function() {
-    //     for (i = 1; i <= 9; i++) {
-        //     if ((`square${i}`).hasClass('x-icon') && ('.square').hasclass('o-icon')) {
-            //         alert("Draw! Try playing again!");
-            //     }
-// }
-// }
+$(function () {
 
 });
